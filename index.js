26d3fd6629deee9bbbc63a6609198ceef3930fa1
David Burrows <david@imergent.com>
2016-05-27 11:07:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logger(error, stdout, stderr) {
   }
 }
 
-exec("npm install webpack webpack-dev-server react react-dom lodash babel-eslint babel-core babel-preset-es2015 babel-preset-react eslint eslint-plugin-react", puts);
+exec("npm install webpack webpack-dev-server react react-dom lodash babel-eslint babel-core babel-preset-es2015 babel-preset-react eslint eslint-plugin-react babel-loader", puts);
 exec("npm init -f", logger);
 exec("mkdir src", logger);
 exec("mkdir test", logger);
