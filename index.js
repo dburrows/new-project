931275747b2106f488e9ade27c0f8778c5b4ed1b
David Burrows <david@imergent.com>
2016-05-27 10:00:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+var fs = require('fs');
 var exec = require('child_process').exec;
 
+// files
 var babelrc = '';
 var gitignore = '';
 var eslintrc = '';
 
-function puts(error, stdout, stderr) {
+// logger
+function logger(error, stdout, stderr) {
   if (error) {
     console.error(`error: ${error}`);
     return;
@@ -12,11 +15,11 @@ function puts(error, stdout, stderr) {
 }
 
 exec("npm install webpack webpack-dev-server react react-dom lodash babel-eslint babel-core babel-preset-es2015 babel-preset-react eslint eslint-plugin-react", puts);
-exec("npm init -f", puts);
-exec("mkdir src", puts);
-exec("mkdir test", puts);
+exec("npm init -f", logger);
+exec("mkdir src", logger);
+exec("mkdir test", logger);
 
-fs.writeFile(".babelrc", babelrc, puts);
-fs.writeFile(".gitignore", babelrc, puts);
-fs.writeFile(".eslintrc", babelrc, puts);
+fs.writeFile(".babelrc", babelrc, logger);
+fs.writeFile(".gitignore", babelrc, logger);
+fs.writeFile(".eslintrc", babelrc, logger);
 
