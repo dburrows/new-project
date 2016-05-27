a411eaa07d9e1cc547117e401e565bb34ee07954
David Burrows <david@imergent.com>
2016-05-27 11:20:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,69 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 
 // files
-var babelrc = '';
-var gitignore = '';
-var eslintrc = '';
-var webpackConfig = '';
+var babelrc = '
+{
+  "presets": ["es2015", "react"]
+}
+';
+
+var gitignore = '
+node_modules
+npm-debug.log
+.DS_Store
+dist
+';
+
+var eslintrc = '
+{
+  "ecmaFeatures": {
+    "jsx": true,
+    "modules": true
+  },
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "parser": "babel-eslint",
+  "rules": {
+    "semi": [2, "always"],
+    "quotes": 0,
+    "no-console": 0,
+    "strict": [2, "never"],
+    "react/jsx-uses-react": 2,
+    "react/jsx-uses-vars": 2,
+    "react/react-in-jsx-scope": 2
+  },
+  "plugins": [
+    "react"
+  ]
+}
+';
+
+var webpackConfig = "
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
+";
 
 // logger
 function logger(error, stdout, stderr) {
